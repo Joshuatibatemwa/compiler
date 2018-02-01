6081f6f14122e725e3732d868b6f851ace5f9078
Joshuatibatemwa <tibatemw@grinnell.edu>
2018-01-31 20:17:43 -0600

    Finished CLI file

--- CLI.ml
+++ CLI.ml
@@ -1,22 +1,29 @@
 let length_flag=ref false
+let ans=ref []
 
-let print_length(x: string)=
-Printf.printf "%d\n" (String.length x)
+let param_list(s: string)=
+	ans := s :: !ans
 
-let print_items =
-match !length_flag with
-|false -> print_endline
-|true -> print_length
+let print_length()=
+	List.iter (Printf.printf "%d \n") (List.map String.length (List.rev !ans))
+
+let print_items()=
+	List.iter print_endline (List.rev !ans)
 
 let main =
-let speclist = [("-length", Arg.Set length_flag, "Prints length of all arguments");
-] in 
-let usage_msg= "Usage: my-project [flags] [args] \n Available flags:" 
-in
- 
-Arg.parse speclist print_items usage_msg
+	let speclist = [("-length", Arg.Set length_flag, "Prints length of all arguments");
+	] in 
+	let usage_msg= "Usage: my-project [flags] [args] \n Available flags:" 
+	in Arg.parse speclist param_list usage_msg;
+	
+		match !length_flag with
+		|false->print_items()
+		|true->print_length()
 
-let ()= main
+
+let _= main
+
+
 
 
 
