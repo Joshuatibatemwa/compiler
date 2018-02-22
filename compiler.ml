2dd37c69b23f5881bb9a1cdd2db859958f3c1fdf
Joshua Tibatemwa <tibatemw@cs.grinnell.edu>
2018-02-22 15:57:22 -0600

    Parsing working

--- compiler.ml
+++ compiler.ml
@@ -1,12 +1,38 @@
 open Lang
+open Lexer
+open Parser
 
-let main () =
-  let filename = Sys.argv.(1) in
-  let tokens   = Lexer.lex (Stream.of_channel (open_in filename)) in
-  let (e, _)   = Parser.parse tokens in
+let lex= ref false
+let parse=ref false
+let tokens =ref[]
+
+let lex_parse(file:string)=
+  let filename= file in
+  tokens := Lexer.lex(Stream.of_channel(open_in filename))
+
+let compile(e:exp):string =
   match e with
-  |EInequality(e1,e2) ->Lang.ineq_interpret e |> string_of_bool |> print_endline
-  |EBoolean bool     -> Lang.ineq_interpret e |> string_of_bool |> print_endline
-  |_                  ->Lang.interpret e |> string_of_int |> print_endline
+  |EInequality(e1,e2) ->Lang.ineq_interpret e |> string_of_bool
+  |EBoolean bool     -> Lang.ineq_interpret e |> string_of_bool
+  |_                  ->Lang.interpret e |> string_of_int 
 
+                      
+let string_of_token_list(list:token list):string=
+  match list with
+  |x::xs -> string_of_token x; string_of_token_list xs 
+  |[] -> ""
+  
+  
+let main () =
+   let speclist = [("-lex", Arg.Set lex, "Prints length of all arguments");
+                       ("-parse", Arg.Set parse, "Prints length of all arguments");]
+        in 
+	let usage_msg= "Usage: my-project [flags] [args] \n Available flags:" in
+        Arg.parse speclist lex_parse usage_msg;
+        match !lex,!parse with
+        |true,_   ->Printf.printf("[("); Printf.printf "%s" (string_of_token_list !tokens);print_endline("]")
+        |_,true   -> print_endline(string_of_token_list !tokens)
+
+              
 let _ = if !Sys.interactive then () else main ()
+  
