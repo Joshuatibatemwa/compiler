c44dcabd2a3415daf6e24770622feef7c86a6ab6
Joshua Tibatemwa <tibatemw@cs.grinnell.edu>
2018-02-25 17:37:20 -0600

    Worked with testing for a bit

--- Testing.sh
+++ Testing.sh
@@ -1,5 +1,18 @@
 
 ocamlc -o cli "CLI.ml"
+ocamlopt -c lexer.ml
+ocamlopt -c lang.ml
+ocamlopt -c parser.ml
+ocamlopt -c compiler.ml
+ocamlopt -o compiler lexer.cmx lang.cmx parser.cmx compiler.cmx
+
+
 
 ./test_output.sh > results.out
 diff solutions.out results.out
+
+
+./test_parse_lex.sh > test_parse_lex.out
+diff parse_lex_solutions.out test_parse_lex.out
+
+
